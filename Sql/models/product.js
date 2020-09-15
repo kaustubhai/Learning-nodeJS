8e2d33b614e6f95164e66388a958e8ffd50c80ff
kaustubh <kaustubh229@gmail.com>
2020-09-16 02:29:29 +0530

    New Chapter

--- Sql/models/product.js
+++ Sql/models/product.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const Cart = require('./cart')
+
+const Cart = require('./cart');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -30,20 +31,33 @@ module.exports = class Product {
   save() {
     getProductsFromFile(products => {
       if (this.id) {
-        const existingProductId = products.findIndex(product => product.id === this.id)
-        const updatedProduct = [...products];
-        updatedProduct[existingProductId] = this;
-        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
           console.log(err);
-        })
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
       }
-      else{
-    this.id = Math.random().toString();
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
       });
-    }
     });
   }
 
@@ -56,17 +70,5 @@ module.exports = class Product {
       const product = products.find(p => p.id === id);
       cb(product);
     });
-  }
-
-  static deleteProduct(id) {
-    getProductsFromFile(products => {
-      const pro = products.find(p => p.id === id)
-      const updatedProduct = products.filter(product => product.id !== id)
-      fs.writeFile(p, JSON.stringify(updatedProduct), err => {
-        if (!err) {
-           Cart.deleteProduct(id, pro.price)
-        }
-      })
-    })
   }
 };
